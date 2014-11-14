847a0844393cc9be3334f1d686797e4831a4011c
ros_feshaffer <ros_feshaffer@WROSRM21727.seattleschools.org>
2014-11-14 14:57:50 -0800

    Set up public class Driving. Set up Jaguar motors, cat1 and cat2. With ports(not final port)

---
+++ src/RebuildBot/Driving.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package RebuildBot;
+import edu.wpi.first.wpilibj.Jaguar;
+/**
+ *
+ * @author ros_feshaffer
+ */
+public class Driving {
+    public Driving(){
+        int port1= 8;//not real port
+        int port2= 4;//not real port
+        Jaguar cats1= new Jaguar(port1);
+        Jaguar cats2= new Jaguar(port2); 
+    }
+    //motors
+    //forward
+    //backwards
+    //turn left
+    //turn right
+    
+}
